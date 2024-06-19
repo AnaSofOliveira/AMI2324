b2c887e165a5fe48826f639d38eae818c124c961
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2024-06-19 21:13:03 +0100

    update

---
+++ TrabalhosPraticos/Gamebook/src/app/registo/registo.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registo',
+  templateUrl: './registo.page.html',
+  styleUrls: ['./registo.page.scss'],
+})
+export class RegistoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
