a068e995654e95560074a8d28ca118880048ff2c
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2024-07-07 23:52:57 +0100

    ➕Adicionada validação de user logado

--- TrabalhosPraticos/Gamebook/src/app/auth/splash/splash.page.ts
+++ TrabalhosPraticos/Gamebook/src/app/auth/splash/splash.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthLayoutComponent } from 'src/app/layout/auth-layout/auth-layout.component';
 import { IonContent, IonSpinner } from "@ionic/angular/standalone";
 import { Router } from '@angular/router';
+import { FireAuthService } from 'src/app/core/services/auth/fire-auth.service';
 
 @Component({
   selector: 'app-splash',
@@ -10,11 +11,19 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [IonSpinner, IonContent, AuthLayoutComponent],
 })
-export class SplashPage {
+export class SplashPage implements OnInit{
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private fireAuthService: FireAuthService) { }
 
   goToSocialLoginPage() {
     this.router.navigate(['/login-social'], { replaceUrl: true });
+  }
+
+  ngOnInit(): void {
+    this.fireAuthService.isLogIn()
+      .then((value) => {
+        value ? this.router.navigate(['/home'], { replaceUrl: true }) : this.goToSocialLoginPage();
+      })
+      .catch(() => this.goToSocialLoginPage());
   }
 }
