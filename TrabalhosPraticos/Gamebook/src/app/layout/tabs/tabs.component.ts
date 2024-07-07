952476869ecf5758d10601364f13d00c390a5005
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2024-07-07 21:27:19 +0100

    🔧 Corrigidas linkagens do menu tabs

--- TrabalhosPraticos/Gamebook/src/app/layout/tabs/tabs.component.ts
+++ TrabalhosPraticos/Gamebook/src/app/layout/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent{
 
-  constructor() { }
+  constructor(private router: Router) { }
 
 }
